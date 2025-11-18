e1178446d3cc3d560603b29b2cb62dda9a68d883
Shirako <c0a2412135@edu.teu.ac.jp>
2025-11-18 16:49:06 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -18,6 +18,7 @@ def main():
             if event.type == pg.QUIT: return
         x = tmr
         screen.blit(bg_img,[-x,0])
+        screen.blit(bg_img,[-x+1600,0])
         screen.blit(kk_img,[300,200])
         screen.blit(bg_img, [0, 0])
         pg.display.update()
