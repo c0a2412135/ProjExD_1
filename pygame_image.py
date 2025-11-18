a1384dc4a1df6161c9ab28f3dc226e945a11559c
Shirako <c0a2412135@edu.teu.ac.jp>
2025-11-18 16:45:43 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -22,7 +22,7 @@ def main():
         screen.blit(bg_img, [0, 0])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
