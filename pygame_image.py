2350c393c034d07f40d843c476fa0ab527be6e3e
Shirako <c0a2412135@edu.teu.ac.jp>
2025-11-18 16:56:35 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -10,8 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.rotozoom(kk_img,True,False)
+    kk_img = pg.transform.flip(pg.image.load("fig/3.png"),True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -20,7 +19,6 @@ def main():
         screen.blit(bg_img,[-x,0])
         screen.blit(bg_img,[-x+1600,0])
         screen.blit(kk_img,[300,200])
-        screen.blit(bg_img, [0, 0])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
